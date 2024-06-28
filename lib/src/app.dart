b56314bc2ae3f46ab472147671191074bc53cb63
CampCounty <camp-county@gmx.de>
2024-06-28 14:47:35 +0200

    auth1

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hot_spot/src/data/auth_repository.dart';
 import 'package:hot_spot/src/data/database_repository.dart';
+import 'package:hot_spot/src/features/authentication/home_screen.dart';
 import 'package:hot_spot/src/features/authentication/presentation/add_fang.dart';
 import 'package:hot_spot/src/features/authentication/presentation/login_screen.dart';
+import 'package:hot_spot/src/features/authentication/presentation/sign_up_screen.dart';
 
 class App extends StatelessWidget {
   final DatabaseRepository databaseRepository;
@@ -15,8 +17,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AddFang(
+      home: HomeScreen(
         databaseRepository: databaseRepository,
+        authRepository: authRepository,
       ),
     );
   }
